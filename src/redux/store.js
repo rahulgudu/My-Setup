0eb6c2f67f2684966ecca7e9faab4ce37c46a83a
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-10-06 12:02:01 +0530

    added the redux reducers and api in login page

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { testReducer } from "./reducers/testReducer";
+import { testAPI } from "./api/testApi";
 export const store = configureStore({
   reducer: {
     [testReducer.name]: testReducer.reducer,
+    [testAPI.reducerPath]: testAPI.reducer,
   },
-  middleware: (mid) => [...mid()],
+  middleware: (mid) => [...mid(), testAPI.middleware],
 });
